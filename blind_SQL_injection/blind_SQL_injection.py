f18ad7ea06df5d3a2c32277c7ee4258996f651d9
pk-coding <159196782+pk-coding@users.noreply.github.com>
2024-07-26 16:23:03 +0200

    Update blind_SQL_injection.py

--- blind_SQL_injection/blind_SQL_injection.py
+++ blind_SQL_injection/blind_SQL_injection.py
@@ -5,9 +5,9 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 dictionary = 'zxcvbnmasdfghjklqwertyuiopZXCVBNMASDFGHJKLQWERTYUIOP1234567890_-!@#$,.;:<>/'
 haslo = ""
-MyProxy = {'http':'http://127.0.0.1:8080/',
-'https':'http://127.0.0.1:8080'}
+MyProxy = {'http':'http://127.0.0.1:8080/', 'https':'http://127.0.0.1:8080'}
 r = requests.Session()
+
 def brute():
     global haslo
     global dictionary
